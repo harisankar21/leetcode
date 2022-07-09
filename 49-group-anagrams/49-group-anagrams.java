669f97cd888cd93179b866a5975e00827578f2b4
harisankar21 <sankarhari778@gmail.com>
2022-07-09 12:05:47 +0530

    Time: 27 ms (26.24%), Space: 61.9 MB (14.25%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+               List<List<String>>  anagrams = new ArrayList<>();
+ List<String> words = Arrays.asList(strs); 
+        // base case
+        if (words == null) {
+            return anagrams;
+        }
+ 
+        // sort each word on the list
+        // List<String> list = words.stream()
+        //         .map(s -> Stream.of(s.split("")).sorted()
+        //                 .collect(Collectors.joining()))
+        //         .collect(Collectors.toList());
+        List<String> list= new ArrayList<>();
+        for(String tmp:words){
+            char[] tmpArray=tmp.toCharArray();
+            Arrays.sort(tmpArray);
+            list.add(new String(tmpArray));
+                
+        }
+ 
+        // construct a map where the key is each sorted word,
+        // and value is a list of indices where it is present
+        Map<String, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < list.size(); i++)
+        {
+            map.putIfAbsent(list.get(i), new ArrayList<>());
+            map.get(list.get(i)).add(i);
+        }
+ 
+        // traverse the map and read indices for each sorted key.
+        // The anagrams are present in the actual list at those indices
+        for (var entry: map.entrySet())
+        {
+            List<String> collection = entry.getValue().stream()
+                    .map(i -> words.get(i))
+                    .collect(Collectors.toList());
+            if (collection.size() >=1) {
+                anagrams.add(collection);
+            }
+        }
+ 
+        return anagrams;
+        
+    }
+}
